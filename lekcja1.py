803e3f9c8df1ce9243ce97e02d8f59b50e31b876
Szymon <100271667+Otlos2002@users.noreply.github.com>
2022-10-04 17:51:19 +0200

    Create lekcja1.py

---
+++ lekcja1.py
@@ -0,0 +1,116 @@
+#zad1
+# A = [1+x for x in range(0,10)]
+# # print(a)
+# B = [2*x for x in range(0,10+1)]
+# # print(B)
+# C = [x**2 for x in range(0,10)]
+# # print(C)
+# D = [x*0 for x in range(0,10)]
+# # print(D)
+# E = [x % 2 for x in range(0,11)]
+# # print(E)
+# F = [x %5 for x in range(0,11)]
+# # print(F)
+
+#zad2
+# a = []
+# x=1
+# while x in range(0,11):
+#     a.append(x)
+#     x+=1
+# print(a)
+
+# a = []
+# x=1
+# while x in range(0,11):
+#     a.append(x*2)
+#     x+=1
+# print(a)
+
+# a = []
+# x=1
+# while x in range(0,11):
+#     a.append(x**2)
+#     x+=1
+# print(a)
+
+# a = []
+# x=1
+# while x in range(0,11):
+#     a.append(x*0)
+#     x+=1
+# print(a)
+
+# a = []
+# x=0
+# while x in range(0,11):
+#     a.append(x%2)
+#     x+=1
+# print(a)
+
+# a = []
+# x=1
+# while x in range(0,10):
+#     a.append(x%5)
+#     x+=1
+# print(a)
+
+# zad3
+# def ile_ujemnych(lista):
+#     licznik=0
+#     for x in lista:
+#         if(x<0):
+#             licznik+=1
+#     print(licznik)
+#
+# list = [-1,2,3,-1,-5,-6,-5,-9]
+# ile_ujemnych(list)
+
+#zad4
+# def iloczyn(lista):
+#     wynik=1
+#     for x in lista:
+#        wynik = wynik*x
+#
+#     print(wynik)
+#
+# list = [5, 2, 3]
+# iloczyn(list)
+#
+#zad5
+# def minmax(list):
+#     M = max(list)
+#     m = min(list)
+#     return (m,M)
+#
+# lista=[1,2,6,8,-3]
+# print(minmax(lista))
+#zad6
+# def sumens(list):
+#     sign = 1
+#     suma = 0
+#     for x in list:
+#         suma += sign*x
+#         sign = -sign
+#     print(suma)
+# 
+# lista=[1 ,4 ,16 ,9 ,9, 7, 4, 9, 11]
+# sumens(lista)
+
+#zad7
+# list=[]
+# i=1
+# var=0
+# x=input("Podaj ")
+# list.append(x)
+# for x in list
+#     i++
+
+
+
+
+
+
+
+
+
